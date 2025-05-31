d70858ddbeaa0717d38bc7489997dc9c5bab360b
Aishwariya Ghosal <aishwariya.ghosal04@gmail.com>
2025-05-31 14:59:56 +0530

    deleted copy folders and files

--- routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createOrder, getOrder } = require('../controllers/orderControllers');
-
-router.post('/', createOrder);
-router.get('/:orderId', getOrder);
-
-module.exports = router;
